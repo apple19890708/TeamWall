065daa4230916eb9ea5ad123fb4259611ef56745
Tommy <tommychen@applyeasypro.com>
2023-04-01 00:26:25 +0800

    [feat] change api_url

--- src/views/Foreground/ChatRoomListView.vue
+++ src/views/Foreground/ChatRoomListView.vue
@@ -24,7 +24,7 @@ const queryRoomList = async () => {
     } = res;
     if (status) {
       Object.assign(chatList, chatRecord);
-      // console.log('chatList', chatList);
+      console.log('chatList', chatList);
     }
   } catch (error) {
     const msg = error.response.data?.message;
